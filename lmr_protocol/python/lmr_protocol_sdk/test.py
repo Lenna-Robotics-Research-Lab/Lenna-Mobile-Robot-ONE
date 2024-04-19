356e24f0fa234df418e2ffd3d565af9db85dfca4
Erfan Riazati <erf.riazati@gmail.com>
2024-04-20 01:11:57 +0430

    :sparkles: [FEATURE] : Add a class for serial frame handler and serial transmit

--- lmr_protocol/python/lmr_protocol_sdk/test.py
+++ lmr_protocol/python/lmr_protocol_sdk/test.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from serial_handler import *
+from packet_handler import *
 
 DEVICENAME = '/dev/ttyTHS1'
 BAUDRATE = 115200
@@ -9,7 +10,12 @@ BAUDRATE = 115200
 serial = SerialHandler(DEVICENAME, BAUDRATE)
 serial.openPort()
 
-txpacket = [0xFF, 0xFF, 0x00, 0x02, 0x0F, 0x0F, 0xAB, 0xCD]
+packet = PacketHandler()
 
 while True:
-    serial.writePort(txpacket)
+    res = packet.txPacket(serial, 0xA0, [0xABCD])
+
+# txpacket = [0xFF, 0xFF, 0x00, 0x02, 0x0F, 0x0F, 0xAB, 0xCD]
+
+# while True:
+#     serial.writePort(txpacket)
