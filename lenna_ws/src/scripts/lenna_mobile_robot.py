b64f7ddebcf7bdba73ad2c947320d5899d40e8d8
Erfan Riazati <erf.riazati@gmail.com>
2024-05-21 21:16:03 +0330

    :sparkles: [FEATURE] : Reading serial data signed

--- lenna_ws/src/scripts/lenna_mobile_robot.py
+++ lenna_ws/src/scripts/lenna_mobile_robot.py
@@ -3,10 +3,35 @@
 
 from field_ops import *
 from packet_handler import *
+from field_ops import *
 
 class LennaMobileRobot():
     def __init__(self, protocol):
         self.protocol = protocol
+        self.odometry_length = 16
 
     def setMotorSpeed(self, motor_speed_left, motor_speed_right):
         return self.protocol.txPacket(INST_MOTION_CONTROL, [motor_speed_left, motor_speed_right])
+
+    def getOdometry(self, timeout=100):
+        data, length, result = self.protocol.rxPacket(timeout)
+
+        heading = 0
+        motor_speed_right = 0
+        motor_speed_left = 0
+        acc_x = 0
+        acc_y = 0
+        gyro_x = 0
+
+        if result == COMM_SUCCESS:
+            if length == self.odometry_length:
+                heading = combine2Byte(data[3], data[2])
+                motor_speed_left = combine2Byte(data[5], data[4])
+                motor_speed_right = combine2Byte(data[7], data[6])
+                acc_x = combine2Byte(data[9], data[8])
+                acc_y = combine2Byte(data[11], data[10])
+                gyro_x = combine2Byte(data[13], data[12])
+            else:
+                result = COMM_RX_FAIL
+
+        return [heading, motor_speed_left, motor_speed_right, acc_x, acc_y, gyro_x], length, result
---
+++ lenna_ws/src/scripts/readtest.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from serial_handler import *
+from packet_handler import *
+from field_ops import *
+from lenna_mobile_robot import *
+
+DEVICENAME = '/dev/ttyTHS1'
+BAUDRATE = 115200
+
+serial = SerialHandler(DEVICENAME, BAUDRATE)
+serial.openPort()
+
+packet = PacketHandler(serial)
+lenna = LennaMobileRobot(packet)
+
+while True:
+    odom, _, _ = lenna.getOdometry(20)
+    print(getSigned(odom))
