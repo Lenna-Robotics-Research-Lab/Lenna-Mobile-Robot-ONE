813d667a959af03f1c1e265b38d7c4a2b35814f9
Erfan Riazati <erf.riazati@gmail.com>
2024-05-25 20:19:27 +0330

    :sparkles: [FEATURE] : Add LiDAR package to the catkin workspace

---
+++ lenna_ws/build/ldlidar_stl_ros/CMakeFiles/ldlidar_stl_ros_node.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ldlidar_stl_ros_node.dir/src/publish_node/main.cpp.o"
+  "CMakeFiles/ldlidar_stl_ros_node.dir/include/ldlidar_driver/cmd_interface_linux.cpp.o"
+  "CMakeFiles/ldlidar_stl_ros_node.dir/include/ldlidar_driver/lipkg.cpp.o"
+  "CMakeFiles/ldlidar_stl_ros_node.dir/include/ldlidar_driver/tofbf.cpp.o"
+  "/home/lenna/repositories/Lenna-Mobile-Robot-ONE/lenna_ws/devel/.private/ldlidar_stl_ros/lib/ldlidar_stl_ros/ldlidar_stl_ros_node.pdb"
+  "/home/lenna/repositories/Lenna-Mobile-Robot-ONE/lenna_ws/devel/.private/ldlidar_stl_ros/lib/ldlidar_stl_ros/ldlidar_stl_ros_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ldlidar_stl_ros_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lenna_ws/devel/share/ldlidar_stl_ros/cmake/ldlidar_stl_rosConfig.cmake
@@ -0,0 +1,1 @@
+/home/lenna/repositories/Lenna-Mobile-Robot-ONE/lenna_ws/devel/.private/ldlidar_stl_ros/share/ldlidar_stl_ros/cmake/ldlidar_stl_rosConfig.cmake
