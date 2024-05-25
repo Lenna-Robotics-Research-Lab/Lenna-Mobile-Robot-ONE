f9f80bcd927e9047d21d88fd1e289e2b7d55055e
Erfan Riazati <erf.riazati@gmail.com>
2024-05-25 19:58:08 +0330

    :fire: [REMOVE] : Remove odometry read and motor speed write test scripts

--- lenna_ws/src/lenna_teleop/scripts/odom_publisher.py
+++ lenna_ws/src/lenna_teleop/scripts/odom_publisher.py
@@ -8,5 +8,19 @@ import numpy as np
 
 from serial_handler import *
 from packet_handler import *
+from field_ops import *
 from lenna_mobile_robot import *
 
+DEVICENAME = '/dev/ttyTHS1'
+BAUDRATE = 115200
+
+serial = SerialHandler(DEVICENAME, BAUDRATE)
+serial.openPort()
+
+packet = PacketHandler(serial)
+lenna = LennaMobileRobot(packet)
+
+while True:
+    pass
+    # odom, _, _ = lenna.getOdometry(20)
+    # print(getSigned(odom))
