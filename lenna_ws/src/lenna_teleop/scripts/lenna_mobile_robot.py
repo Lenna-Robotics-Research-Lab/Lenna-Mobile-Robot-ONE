02b1469e40ad6bdafad3016b48874a48b23e4fd0
Erfan Riazati <erf.riazati@gmail.com>
2024-06-10 21:08:29 +0330

    :sparkle: publishing odometry items

--- lenna_ws/src/lenna_teleop/scripts/lenna_mobile_robot.py
+++ lenna_ws/src/lenna_teleop/scripts/lenna_mobile_robot.py
@@ -8,11 +8,11 @@ from field_ops import *
 class LennaMobileRobot():
     def __init__(self, protocol):
         self.protocol = protocol
-        self.odometry_length = 26
+        self.odometry_length = 32
         self.min_motor_speed = 0
         self.max_motor_speed = 250 # rpm
         self.wheel_radius = 0.0375 # meter
-        self.wheel_distance = 0.20 # meter
+        self.wheel_distance = 0.18 # meter
 
     def setMotorSpeed(self, motor_speed_left, motor_speed_right):
         return self.protocol.txPacket(INST_MOTION_CONTROL, [motor_speed_left, motor_speed_right])
@@ -40,17 +40,25 @@ class LennaMobileRobot():
 
         if result == COMM_SUCCESS:
             if length == self.odometry_length:
-                yaw = combine2Byte(data[5], data[4])
-                left_wheel_speed = combine2Byte(data[7], data[6])
-                right_wheel_speed = combine2Byte(data[9], data[8])
-                acc_x = combine2Byte(data[11], data[10])
-                acc_y = combine2Byte(data[13], data[12])
-                gyro_x = combine2Byte(data[15], data[14])
-                gyro_y = combine2Byte(data[17], data[16])
-                gyro_z = combine2Byte(data[19], data[18])
-                right_wheel_distance = combine2Byte(data[21], data[20])
-                left_wheel_distance = combine2Byte(data[23], data[22])
+                left_wheel_speed = combine2Byte(data[5], data[4])
+                right_wheel_speed = combine2Byte(data[7], data[6])
+
+                left_wheel_distance = combine2Byte(data[9], data[8])
+                right_wheel_distance = combine2Byte(data[11], data[10])
+
+                acc_x = combine2Byte(data[13], data[12])
+                acc_y = combine2Byte(data[15], data[14])
+                acc_z = combine2Byte(data[17], data[16])
+
+                gyro_x = combine2Byte(data[19], data[18])
+                gyro_y = combine2Byte(data[21], data[20])
+                gyro_z = combine2Byte(data[23], data[22])
+
+                roll = combine2Byte(data[25], data[24])
+                pitch = combine2Byte(data[27], data[26])
+                yaw = combine2Byte(data[29], data[28])
+
             else:
                 result = COMM_RX_FAIL
 
-        return [yaw, left_wheel_speed, right_wheel_speed, acc_x, acc_y, gyro_x, gyro_y, gyro_z, right_wheel_distance, left_wheel_distance], length, result
+        return [left_wheel_speed, right_wheel_speed, left_wheel_distance, right_wheel_distance, acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z, roll, pitch, yaw], length, result
--- lenna_ws/src/lenna_teleop/scripts/odom_publisher.py
+++ lenna_ws/src/lenna_teleop/scripts/odom_publisher.py
@@ -5,6 +5,7 @@ from nav_msgs.msg import Odometry
 from sensor_msgs.msg import Imu
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Quaternion
+from geometry_msgs.msg import Pose
 
 import numpy as np
 
@@ -30,19 +31,19 @@ if __name__ == "__main__":
     imu_data = Imu()
 
     while not rospy.is_shutdown():
-        odom, _, _ = lenna.getOdometry(10)
+        odom, _, _ = lenna.getOdometry(100)
         odom = getSigned(odom) 
 
         imu_data.header.stamp = rospy.Time.now()
         imu_data.header.frame_id = 'imu_sensor'
 
-        imu_data.linear_acceleration.x = odom[3]/ 1000
-        imu_data.linear_acceleration.y = odom[4]/ 1000
-        imu_data.linear_acceleration.z = 0
+        imu_data.linear_acceleration.x = odom[4] / 1000
+        imu_data.linear_acceleration.y = odom[5] / 1000
+        imu_data.linear_acceleration.z = odom[6] / 1000
 
-        imu_data.angular_velocity.x = odom[5]/ 1000
-        imu_data.angular_velocity.y = odom[6]/ 1000
-        imu_data.angular_velocity.z = odom[7]/ 1000
+        imu_data.angular_velocity.x = odom[7] / 1000
+        imu_data.angular_velocity.y = odom[8] / 1000
+        imu_data.angular_velocity.z = odom[9] / 1000
         
         imu_pub.publish(imu_data)
         rospy.loginfo("All went well. Publishing topic /imu_data")
