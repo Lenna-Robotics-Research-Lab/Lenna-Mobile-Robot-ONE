ee81edce4b3d62eadaa76080c7fc2e1baa4d2ba7
Erfan Riazati <erf.riazati@gmail.com>
2024-06-10 15:49:15 +0330

    ✨ [FEATURE] : Recieve IMU Date from Serial + Publish with the Type sensor_msgs.Imu

--- lenna_ws/src/lenna_teleop/scripts/odom_publisher.py
+++ lenna_ws/src/lenna_teleop/scripts/odom_publisher.py
@@ -2,7 +2,9 @@
 
 import rospy
 from nav_msgs.msg import Odometry
+from sensor_msgs.msg import Imu
 from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Quaternion
 
 import numpy as np
 
@@ -20,7 +22,28 @@ serial.openPort()
 packet = PacketHandler(serial)
 lenna = LennaMobileRobot(packet)
 
-while True:
-    pass
-    # odom, _, _ = lenna.getOdometry(20)
-    # print(getSigned(odom))
+if __name__ == "__main__":
+    rospy.init_node('node_odom_imu', anonymous=False)
+    imu_pub = rospy.Publisher('/imu_data', Imu, queue_size=10)
+    rate = rospy.Rate(100)
+
+    imu_data = Imu()
+
+    while not rospy.is_shutdown():
+        odom, _, _ = lenna.getOdometry(10)
+        odom = getSigned(odom) 
+
+        imu_data.header.stamp = rospy.Time.now()
+        imu_data.header.frame_id = 'imu_sensor'
+
+        imu_data.linear_acceleration.x = odom[3]/ 1000
+        imu_data.linear_acceleration.y = odom[4]/ 1000
+        imu_data.linear_acceleration.z = 0
+
+        imu_data.angular_velocity.x = odom[5]/ 1000
+        imu_data.angular_velocity.y = odom[6]/ 1000
+        imu_data.angular_velocity.z = odom[7]/ 1000
+        
+        imu_pub.publish(imu_data)
+        rospy.loginfo("All went well. Publishing topic /imu_data")
+        rate.sleep()
