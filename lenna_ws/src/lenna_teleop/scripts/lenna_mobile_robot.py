412f040d2ce47ba3648f7f90ff8d10c3468b1788
Erfan Riazati <erf.riazati@gmail.com>
2024-06-10 15:45:55 +0330

    ✨ [ADD] : Odometry RX Packet Handler

--- lenna_ws/src/lenna_teleop/scripts/lenna_mobile_robot.py
+++ lenna_ws/src/lenna_teleop/scripts/lenna_mobile_robot.py
@@ -8,9 +8,9 @@ from field_ops import *
 class LennaMobileRobot():
     def __init__(self, protocol):
         self.protocol = protocol
-        self.odometry_length = 16
+        self.odometry_length = 26
         self.min_motor_speed = 0
-        self.max_motor_speed = 300 # rpm
+        self.max_motor_speed = 250 # rpm
         self.wheel_radius = 0.0375 # meter
         self.wheel_distance = 0.20 # meter
 
@@ -20,22 +20,37 @@ class LennaMobileRobot():
     def getOdometry(self, timeout=100):
         data, length, result = self.protocol.rxPacket(timeout)
 
-        heading = 0
-        motor_speed_right = 0
-        motor_speed_left = 0
+        right_wheel_speed = 0
+        left_wheel_speed = 0
+
+        right_wheel_distance = 0
+        left_wheel_distance = 0
+
+        roll = 0
+        pitch = 0
+        yaw = 0        
+        
+        gyro_x = 0
+        gyro_y = 0
+        gyro_z = 0
+
         acc_x = 0
         acc_y = 0
-        gyro_x = 0
+        acc_z = 0
 
         if result == COMM_SUCCESS:
             if length == self.odometry_length:
-                heading = combine2Byte(data[3], data[2])
-                motor_speed_left = combine2Byte(data[5], data[4])
-                motor_speed_right = combine2Byte(data[7], data[6])
-                acc_x = combine2Byte(data[9], data[8])
-                acc_y = combine2Byte(data[11], data[10])
-                gyro_x = combine2Byte(data[13], data[12])
+                yaw = combine2Byte(data[5], data[4])
+                left_wheel_speed = combine2Byte(data[7], data[6])
+                right_wheel_speed = combine2Byte(data[9], data[8])
+                acc_x = combine2Byte(data[11], data[10])
+                acc_y = combine2Byte(data[13], data[12])
+                gyro_x = combine2Byte(data[15], data[14])
+                gyro_y = combine2Byte(data[17], data[16])
+                gyro_z = combine2Byte(data[19], data[18])
+                right_wheel_distance = combine2Byte(data[21], data[20])
+                left_wheel_distance = combine2Byte(data[23], data[22])
             else:
                 result = COMM_RX_FAIL
 
-        return [heading, motor_speed_left, motor_speed_right, acc_x, acc_y, gyro_x], length, result
+        return [yaw, left_wheel_speed, right_wheel_speed, acc_x, acc_y, gyro_x, gyro_y, gyro_z, right_wheel_distance, left_wheel_distance], length, result
