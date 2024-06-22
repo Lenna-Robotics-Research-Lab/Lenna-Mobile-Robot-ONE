36eab360cb65630e4157783bf501a575e38e633b
Erfan Riazati <erf.riazati@gmail.com>
2024-06-22 17:50:31 +0330

    :sparkles: [ADD] : Seperate IMU and Encoder Read Scripts

---
+++ lenna_ws/src/lenna_teleop/scripts/nav_publisher.py
@@ -0,0 +1,166 @@
+#!/usr/bin/python3
+
+import rospy
+
+from nav_msgs.msg import Odometry
+from sensor_msgs.msg import Imu
+from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Quaternion
+from geometry_msgs.msg import Pose
+
+import numpy as np
+import math
+
+from serial_handler import *
+from packet_handler import *
+from field_ops import *
+from lenna_mobile_robot import *
+
+import time
+
+DEVICENAME = '/dev/ttyTHS1'
+BAUDRATE = 115200
+
+serial = SerialHandler(DEVICENAME, BAUDRATE)
+serial.openPort()
+
+packet = PacketHandler(serial)
+lenna = LennaMobileRobot(packet)
+
+odom = Odometry()
+odom_old = Odometry()
+
+
+def update_odom(left_vel, right_vel, left_dist, right_dist, old_l, old_r):
+    left_dist /= 100
+    right_dist /= 100
+
+    old_l /= 100
+    old_r /= 100
+
+    left_vel = (left_vel / 60) * (2*np.pi*lenna.wheel_radius)
+    right_vel = (right_vel / 60) * (2*np.pi*lenna.wheel_radius)
+
+    avg_dist = (left_dist + right_dist)/2
+    avg_angle = 2 * math.atan((right_dist - left_dist))/lenna.wheel_distance
+
+    d_l = left_dist - old_l
+    d_r = right_dist - old_r
+    
+    d_avg_dist = (d_l + d_r)/2
+    d_th = 2 * math.atan((d_l - d_r))/lenna.wheel_distance
+
+    if (d_th > np.pi) :
+        d_th -= 2*np.pi
+
+    elif (d_th < -np.pi) :
+        d_th += 2*np.pi
+
+    if (avg_angle > np.pi) :
+        avg_angle -= 2*np.pi
+
+    elif (avg_angle < -np.pi) :
+        avg_angle += 2*np.pi
+
+    odom.pose.pose.position.x = odom_old.pose.pose.position.x + np.cos(d_th) * d_avg_dist
+    odom.pose.pose.position.y = odom_old.pose.pose.position.y + np.sin(d_th) * d_avg_dist
+    odom.pose.pose.orientation.z = avg_angle
+    
+    #   Prevent lockup from a single bad cycle
+    if (np.isnan(odom.pose.pose.position.x) or np.isnan(odom.pose.pose.position.y)
+        or np.isnan(odom.pose.pose.position.z)):
+        odom.pose.pose.position.x = odom_old.pose.pose.position.x
+        odom.pose.pose.position.y = odom_old.pose.pose.position.y
+        odom.pose.pose.orientation.z = odom_old.pose.pose.orientation.z
+ 
+    #    Make sure theta stays in the correct range
+    if (odom.pose.pose.orientation.z > np.pi):
+        odom.pose.pose.orientation.z -= 2 * np.pi
+    elif (odom.pose.pose.orientation.z < -np.pi): 
+        odom.pose.pose.orientation.z += 2 * np.pi
+
+    
+    #   // Compute the velocity
+    odom.header.stamp = rospy.Time.now()
+    odom.twist.twist.linear.x = (right_vel + left_vel) / 2
+    odom.twist.twist.angular.z = 2 * (right_vel - left_vel) / lenna.wheel_distance
+ 
+    #   // Save th e pose data for the next cycle
+    odom_old.pose.pose.position.x = odom.pose.pose.position.x
+    odom_old.pose.pose.position.y = odom.pose.pose.position.y
+    odom_old.pose.pose.orientation.z = odom.pose.pose.orientation.z
+    odom_old.header.stamp = odom.header.stamp
+ 
+    #   // Publish the odometry message
+    #   odom_data_pub.publish(odomNew);
+
+
+if __name__ == "__main__":
+    rospy.init_node('node_odom', anonymous=False)
+    my_nav = rospy.Publisher('/odom', Odometry, queue_size=10)
+
+    rate = rospy.Rate(100)
+    
+    odom.pose.pose.position.x = 0 #initial x which I think is 0
+    odom.pose.pose.position.y = 0
+    odom.pose.pose.position.z = 0
+
+    old_l = 0
+    old_r = 0
+
+    while not rospy.is_shutdown():
+        enc, _, _ = lenna.getOdometry(100)
+        enc = getSigned(enc)
+
+        odom.header.frame_id = 'odom'
+        odom.child_frame_id = "base_link"
+        update_odom(enc[0], enc[1], enc[2] , enc[3],old_l,old_r)
+        old_l,old_r = enc[2] ,enc[3]
+        
+        my_nav.publish(odom)
+        rate.sleep()
+
+    #not sure
+    #odom.pose.position.z = 0
+    #odom.pose.orientation
+
+
+
+#      q.setRPY(0, 0, odomNew.pose.pose.orientation.z);
+ 
+#   nav_msgs::Odometry quatOdom;
+#   quatOdom.header.stamp = odomNew.header.stamp;
+#   quatOdom.header.frame_id = "odom";
+#   quatOdom.child_frame_id = "base_link";
+#   quatOdom.pose.pose.position.x = odomNew.pose.pose.position.x;
+#   quatOdom.pose.pose.position.y = odomNew.pose.pose.position.y;
+#   quatOdom.pose.pose.position.z = odomNew.pose.pose.position.z;
+#   quatOdom.pose.pose.orientation.x = q.x();
+#   quatOdom.pose.pose.orientation.y = q.y();
+#   quatOdom.pose.pose.orientation.z = q.z();
+#   quatOdom.pose.pose.orientation.w = q.w();
+#   quatOdom.twist.twist.linear.x = odomNew.twist.twist.linear.x;
+#   quatOdom.twist.twist.linear.y = odomNew.twist.twist.linear.y;
+#   quatOdom.twist.twist.linear.z = odomNew.twist.twist.linear.z;
+#   quatOdom.twist.twist.angular.x = odomNew.twist.twist.angular.x;
+#   quatOdom.twist.twist.angular.y = odomNew.twist.twist.angular.y;
+#   quatOdom.twist.twist.angular.z = odomNew.twist.twist.angular.z;
+  
+
+#  odomNew.header.frame_id = "odom";
+# odomNew.pose.pose.position.z = 0;
+# odomNew.pose.pose.orientation.x = 0;
+# odomNew.pose.pose.orientation.y = 0;
+# odomNew.twist.twist.linear.x = 0;
+# odomNew.twist.twist.linear.y = 0;
+# odomNew.twist.twist.linear.z = 0;
+# odomNew.twist.twist.angular.x = 0;
+# odomNew.twist.twist.angular.y = 0;
+# odomNew.twist.twist.angular.z = 0;
+# odomOld.pose.pose.position.x = initialX;
+# odomOld.pose.pose.position.y = initialY;
+# odomOld.pose.pose.orientation.z = initialTheta;
+
+# // Launch ROS and create a node
+# ros::init(argc, argv, "ekf_odom_pub");
+# ros::NodeHandle node;
--- lenna_ws/src/lenna_teleop/scripts/odom_publisher.py
+++ lenna_ws/src/lenna_teleop/scripts/odom_publisher.py
@@ -14,6 +14,8 @@ from packet_handler import *
 from field_ops import *
 from lenna_mobile_robot import *
 
+import time
+
 DEVICENAME = '/dev/ttyTHS1'
 BAUDRATE = 115200
 
@@ -24,18 +26,31 @@ packet = PacketHandler(serial)
 lenna = LennaMobileRobot(packet)
 
 if __name__ == "__main__":
-    rospy.init_node('node_odom_imu', anonymous=False)
+    rospy.init_node('node_imu', anonymous=False)
+    #the difference between this and the reference code is that this is 
+    #publishing undet IMU and /imu_data and the other one is publishing under
+    #nav_msgs.Odometry
     imu_pub = rospy.Publisher('/imu_data', Imu, queue_size=10)
     rate = rospy.Rate(100)
 
     imu_data = Imu()
+
+    # roll = np.radians(-45)
+    # roll_2 = np.radians(315)
+
+    # orient = lenna.rpy2quat(roll, 0, 0)
+    # orient_2 = lenna.rpy2quat(roll_2, 0, 0)
+    # print(orient)
+    # print(orient_2)
+
+    # time.sleep(20)
 
     while not rospy.is_shutdown():
         odom, _, _ = lenna.getOdometry(100)
         odom = getSigned(odom) 
 
         imu_data.header.stamp = rospy.Time.now()
-        imu_data.header.frame_id = 'imu_sensor'
+        imu_data.header.frame_id = 'imu'
 
         imu_data.linear_acceleration.x = odom[4] / 1000
         imu_data.linear_acceleration.y = odom[5] / 1000
@@ -44,7 +59,21 @@ if __name__ == "__main__":
         imu_data.angular_velocity.x = odom[7] / 1000
         imu_data.angular_velocity.y = odom[8] / 1000
         imu_data.angular_velocity.z = odom[9] / 1000
+
+        # print(f"imu in degree: roll:{1} pitch:{2} yaw:{3}", odom[10], odom[11], odom[12])
+
+        roll = np.radians(odom[10])
+        pitch = np.radians(odom[11])
+        yaw = np.radians(odom[12])
+
+        orientation = lenna.rpy2quat(roll, pitch, yaw)
+
+        imu_data.orientation.x = orientation[0]
+        imu_data.orientation.y = orientation[1]
+        imu_data.orientation.z = orientation[2]
+        imu_data.orientation.w = orientation[3]
         
         imu_pub.publish(imu_data)
-        rospy.loginfo("All went well. Publishing topic /imu_data")
+        # rospy.loginfo("All went well. Publishing topic /imu_data")
         rate.sleep()
+
