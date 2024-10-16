c4648b70c4d8f6c0e941cfaa00883b662b4c540a
Erfan Riazati <erf.riazati@gmail.com>
2024-10-16 21:10:31 +0330

    🐛 [FIX] : Update Odometery Data Using Encoder

--- lenna_ws/src/lenna_teleop/scripts/nav_publisher.py
+++ lenna_ws/src/lenna_teleop/scripts/nav_publisher.py
@@ -28,8 +28,6 @@ packet = PacketHandler(serial)
 lenna = LennaMobileRobot(packet)
 
 odom = Odometry()
-odom_old = Odometry()
-
 
 def update_odom(left_vel, right_vel, left_dist, right_dist, old_l, old_r):
 
@@ -43,72 +41,59 @@ def update_odom(left_vel, right_vel, left_dist, right_dist, old_l, old_r):
     left_vel = (left_vel / 60) * (2*np.pi*lenna.wheel_radius)   # m/s
     right_vel = (right_vel / 60) * (2*np.pi*lenna.wheel_radius) # m/s
 
-    avg_dist = (left_dist + right_dist)/2
-    avg_angle = math.atan((right_dist - left_dist)/lenna.wheel_distance)
+    # avg_dist = (left_dist + right_dist)/2
+    # avg_angle = math.atan((right_dist - left_dist)/lenna.wheel_distance)
+
+    # avg_angle = (right_dist - left_dist) / (lenna.wheel_distance / 2)
 
     # mapping 0~2pi to -pi~pi
-    if (avg_angle > np.pi) :
-        avg_angle -= 2*np.pi
+    # if (avg_angle > np.pi) :
+    #     avg_angle -= 2*np.pi
 
-    elif (avg_angle < -np.pi) :
-        avg_angle += 2*np.pi
+    # elif (avg_angle < -np.pi) :
+    #     avg_angle += 2*np.pi
 
     d_l = left_dist - old_l
     d_r = right_dist - old_r
     
     d_avg_dist = (d_l + d_r)/2
-    d_th = 2 * math.atan((d_r - d_l))/lenna.wheel_distance
+    d_theta = (d_r - d_l) / (lenna.wheel_distance / 2)
 
-    if (d_th > np.pi) :
-        d_th -= 2*np.pi
+    if (d_theta > np.pi) :
+        d_theta -= 2*np.pi
 
-    elif (d_th < -np.pi) :
-        d_th += 2*np.pi
+    elif (d_theta < -np.pi) :
+        d_theta += 2*np.pi
 
-    # print(d_th)
+    x_old = odom.pose.pose.position.x
+    y_old = odom.pose.pose.position.y
+    theta_old = (old_r - old_l) / (lenna.wheel_distance / 2)
 
-    # this is changing rpy to quatrenion how? go find it yourself
-    quatrenion = Quaternion()
+    if (theta_old > np.pi) :
+        theta_old -= 2*np.pi
 
-    quatrenion.x = 0
-    quatrenion.y = 0
-    quatrenion.z = np.sin(avg_angle/2)
-    quatrenion.w = np.cos(avg_angle/2)
+    elif (theta_old < -np.pi) :
+        theta_old += 2*np.pi
 
-    odom.pose.pose.position.x = odom.pose.pose.position.x + np.cos(avg_angle) * d_avg_dist
-    odom.pose.pose.position.y = odom.pose.pose.position.y + np.sin(avg_angle) * d_avg_dist
-    odom.pose.pose.orientation = quatrenion
+    print(x_old, y_old, theta_old, d_avg_dist, d_theta)
 
-    # odom.pose.pose.orientation.z = avg_angle
+    theta_new = theta_old + d_theta
+    x_new = x_old + np.cos(theta_new) * d_avg_dist
+    y_new = y_old + np.sin(theta_new) * d_avg_dist
 
-    
-    #   Prevent lockup from a single bad cycle
-    # if (np.isnan(odom.pose.pose.position.x) or np.isnan(odom.pose.pose.position.y)
-    #     or np.isnan(odom.pose.pose.position.z)):
-    #     odom.pose.pose.position.x = odom_old.pose.pose.position.x
-    #     odom.pose.pose.position.y = odom_old.pose.pose.position.y
-    #     odom.pose.pose.orientation = odom_old.pose.pose.orientation
- 
-    #    Make sure theta stays in the correct range
-    # if (odom.pose.pose.orientation.z > np.pi):
-    #     odom.pose.pose.orientation.z -= 2 * np.pi
-    # elif (odom.pose.pose.orientation.z < -np.pi): 
-    #     odom.pose.pose.orientation.z += 2 * np.pi
-
-    
-    #  Compute the velocity
     odom.header.stamp = rospy.Time.now()
+
+    odom.pose.pose.position.x = x_new
+    odom.pose.pose.position.y = y_new
+    odom.pose.pose.position.z = 0
+
+    odom.pose.pose.orientation.x = 0
+    odom.pose.pose.orientation.y = 0
+    odom.pose.pose.orientation.z = np.sin(theta_new/2)
+    odom.pose.pose.orientation.w = np.cos(theta_new/2)
+    
     odom.twist.twist.linear.x = (right_vel + left_vel) / 2
     odom.twist.twist.angular.z = 2 * (right_vel - left_vel) / lenna.wheel_distance
- 
-    # Save th e pose data for the next cycle
-    odom_old.pose.pose.position.x = odom.pose.pose.position.x
-    odom_old.pose.pose.position.y = odom.pose.pose.position.y
-    odom_old.pose.pose.orientation = odom.pose.pose.orientation
-    odom_old.header.stamp = odom.header.stamp
- 
-    #   Publish the odometry message
-    #   odom_data_pub.publish(odomNew);
 
 
 if __name__ == "__main__":
@@ -117,66 +102,28 @@ if __name__ == "__main__":
 
     rate = rospy.Rate(100)
     
-    odom.pose.pose.position.x = 0 #initial x which I think is 0
+    odom.header.stamp = rospy.Time.now()
+    odom.header.frame_id = 'odom'
+    odom.child_frame_id = "base_link"
+
+    odom.pose.pose.position.x = 0
     odom.pose.pose.position.y = 0
     odom.pose.pose.position.z = 0
+
+    odom.pose.pose.orientation.x = 0
+    odom.pose.pose.orientation.y = 0
+    odom.pose.pose.orientation.z = 0
+    odom.pose.pose.orientation.w = 1
 
     old_l = 0
     old_r = 0
 
     while not rospy.is_shutdown():
-        enc, _, _ = lenna.getOdometry(100)
+        enc, _, _ = lenna.getOdometry()
         enc = getSigned(enc)
 
-        odom.header.frame_id = 'odom'
-        odom.child_frame_id = "base_link"
         update_odom(enc[0], enc[1], enc[2], enc[3], old_l, old_r)
-        old_l,old_r = enc[2] ,enc[3]
+        old_l, old_r = enc[2] ,enc[3]
         
         my_nav.publish(odom)
         rate.sleep()
-
-    #not sure
-    #odom.pose.position.z = 0
-    #odom.pose.orientation
-
-
-
-#      q.setRPY(0, 0, odomNew.pose.pose.orientation.z);
- 
-#   nav_msgs::Odometry quatOdom;
-#   quatOdom.header.stamp = odomNew.header.stamp;
-#   quatOdom.header.frame_id = "odom";
-#   quatOdom.child_frame_id = "base_link";
-#   quatOdom.pose.pose.position.x = odomNew.pose.pose.position.x;
-#   quatOdom.pose.pose.position.y = odomNew.pose.pose.position.y;
-#   quatOdom.pose.pose.position.z = odomNew.pose.pose.position.z;
-#   quatOdom.pose.pose.orientation.x = q.x();
-#   quatOdom.pose.pose.orientation.y = q.y();
-#   quatOdom.pose.pose.orientation.z = q.z();
-#   quatOdom.pose.pose.orientation.w = q.w();
-#   quatOdom.twist.twist.linear.x = odomNew.twist.twist.linear.x;
-#   quatOdom.twist.twist.linear.y = odomNew.twist.twist.linear.y;
-#   quatOdom.twist.twist.linear.z = odomNew.twist.twist.linear.z;
-#   quatOdom.twist.twist.angular.x = odomNew.twist.twist.angular.x;
-#   quatOdom.twist.twist.angular.y = odomNew.twist.twist.angular.y;
-#   quatOdom.twist.twist.angular.z = odomNew.twist.twist.angular.z;
-  
-
-#  odomNew.header.frame_id = "odom";
-# odomNew.pose.pose.position.z = 0;
-# odomNew.pose.pose.orientation.x = 0;
-# odomNew.pose.pose.orientation.y = 0;
-# odomNew.twist.twist.linear.x = 0;
-# odomNew.twist.twist.linear.y = 0;
-# odomNew.twist.twist.linear.z = 0;
-# odomNew.twist.twist.angular.x = 0;
-# odomNew.twist.twist.angular.y = 0;
-# odomNew.twist.twist.angular.z = 0;
-# odomOld.pose.pose.position.x = initialX;
-# odomOld.pose.pose.position.y = initialY;
-# odomOld.pose.pose.orientation.z = initialTheta;
-
-# // Launch ROS and create a node
-# ros::init(argc, argv, "ekf_odom_pub");
-# ros::NodeHandle node;
