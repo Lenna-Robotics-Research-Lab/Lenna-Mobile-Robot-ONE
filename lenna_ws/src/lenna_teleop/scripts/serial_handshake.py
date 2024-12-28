a6eae078f9a4996656e17e7fc243265deba06106
Erfan Riazati <erf.riazati@gmail.com>
2024-12-28 20:00:26 +0330

    🚧 edit : Serial handshake acknowledge packet (Ongoing)

--- lenna_ws/src/lenna_teleop/scripts/serial_handshake.py
+++ lenna_ws/src/lenna_teleop/scripts/serial_handshake.py
@@ -20,27 +20,75 @@ serial.openPort()
 
 packet = PacketHandler(serial)
 
+# hs_feedback_flag = False
+# isfirt_flag = True
+
+# def feedbackSubscriberCallback(data):
+#     global hs_feedback_flag
+#     global isfirt_flag
+#     if data:
+#         hs_feedback_flag = True
+#         isfirt_flag = False
+#     elif not data:
+#         hs_feedback_flag = False
+#     else :
+#         hs_feedback_flag = True
+
 if __name__ == "__main__":
     rospy.init_node('node_handshake', anonymous=False)
     hs_pub = rospy.Publisher('/handshake', Bool, queue_size=10)
+
+    # rospy.Subscriber('/handshake_feedback', Bool, feedbackSubscriberCallback)
+
     rate = rospy.Rate(10)
 
+    flag = 0
     
-    while(True):
-        data = serial.readPort(10)
-        hs_pub.publish(0)
-        if data:
+    while not rospy.is_shutdown():
+        if not flag:
+            data = serial.readPort(5)
+
+        if (data) and (not flag):
+            hs_pub.publish(0)
             rospy.loginfo("handshake data received!")
             if data == b'LENNA':
                 rospy.loginfo("handshake data is correct!") 
                 serial.writePort([0x45])
                 flag = 1
-                break
+                # break
                 # why 45? shinedown song or a holy decimal number???
             else:
                 rospy.loginfo(f"incorrect handshake data : {data}")
 
-    while not rospy.is_shutdown():
-        hs_pub.publish(1)
+        elif flag:
+            hs_pub.publish(1)
+
         rate.sleep()
+
+    # while not rospy.is_shutdown():
+    #     if not hs_feedback_flag and (not isfirt_flag): 
+    #         data = serial.readPort(10)
+    #         hs_pub.publish(0)
+    #         if data:
+    #             rospy.loginfo("handshake data received!")
+    #             if data == b'LENNA':
+    #                 rospy.loginfo("handshake data is correct!") 
+    #                 serial.writePort([0x45])
+    #                 flag = 1
+    #                 # why 45? shinedown song or a holy decimal number???
+    #             else:
+    #                 rospy.loginfo(f"incorrect handshake data : {data}")
+    #         else:
+    #             if flag == 1:
+    #                 rospy.loginfo("connection to the host board is lost")
+    #                 flag = 0
+    #             else: 
+    #                 rospy.loginfo("no data is available")
+    #                 flag = 0
+    #     else:
+    #         flag = 1
+    #         hs_pub.publish(bool(flag))    
+        
+       
+    
         
