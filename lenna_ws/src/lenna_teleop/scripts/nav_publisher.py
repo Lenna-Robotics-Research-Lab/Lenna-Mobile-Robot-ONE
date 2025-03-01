9de77afd1a3ff1430f05a588ebf1557a67f06e8d
Erfan Riazati <erf.riazati@gmail.com>
2025-03-01 15:22:53 +0330

    🐛 [FIX] : Odometery Model Orientation

--- lenna_ws/src/lenna_teleop/scripts/nav_publisher.py
+++ lenna_ws/src/lenna_teleop/scripts/nav_publisher.py
@@ -38,79 +38,30 @@ def update_odom(left_vel, right_vel, left_dist, right_dist, old_l, old_r):
     old_l /= 1000
     old_r /= 1000
 
-    # if left_vel >= 300:
-    #     left_vel = 300
-    # elif left_vel <= -300:
-    #     left_vel = -300
-
-    # if right_vel >= 300:
-    #     right_vel = 300
-    # elif right_vel <= -300:
-    #     right_vel = -300
-
-    vel_r_raw = right_vel
-    vel_l_raw = left_vel
-
     left_vel = (left_vel / 60) * (2*np.pi*lenna.wheel_radius)   # m/s
     right_vel = (right_vel / 60) * (2*np.pi*lenna.wheel_radius) # m/s
 
     d_l = left_dist - old_l
     d_r = right_dist - old_r
 
-    # if d_l >= 0.02:
-    #     d_l = 0.02
-    # elif d_l <= -0.02:
-    #     d_l = -0.02
-
-    # if d_r >= 0.02:
-    #     d_r = 0.02
-    # elif d_r <= -0.02:
-    #     d_r = -0.02 
-
     d_avg_dist = (d_l + d_r)/2
-    d_theta = (d_r - d_l) / (lenna.wheel_distance / 2)
-
-    # if (d_theta > np.pi) :
-    #     d_theta -= 2*np.pi
-
-    # elif (d_theta < -np.pi) :
-    #     d_theta += 2*np.pi
-
+    d_theta = (d_r - d_l) / (lenna.wheel_distance) # removed /2
 
     x_old = odom.pose.pose.position.x
     y_old = odom.pose.pose.position.y
-    theta_old = (old_r - old_l) / (lenna.wheel_distance / 2)
-
-    # if (theta_old > np.pi) :
-    #     theta_old -= 2*np.pi
-
-    # elif (theta_old < -np.pi) :
-    #     theta_old += 2*np.pi
+    theta_old = (old_r - old_l) / (lenna.wheel_distance) # removed /2
 
     theta_new = theta_old + d_theta
     x_new = x_old + np.cos(theta_new) * d_avg_dist
     y_new = y_old + np.sin(theta_new) * d_avg_dist
 
-    odom.header.stamp = rospy.Time.now()
-
-    odom.pose.pose.position.x = x_new
-    odom.pose.pose.position.y = y_new
-    odom.pose.pose.position.z = 0
-
-    odom.pose.pose.orientation.x = 0
-    odom.pose.pose.orientation.y = 0
-    odom.pose.pose.orientation.z = np.sin(theta_new/2)
-    odom.pose.pose.orientation.w = np.cos(theta_new/2)
-    
-    odom.twist.twist.linear.x = (right_vel + left_vel) / 2
-    odom.twist.twist.angular.z = 2 * (right_vel - left_vel) / lenna.wheel_distance
-
+    return x_new, y_new, theta_new
 
 if __name__ == "__main__":
     rospy.init_node('node_odom', anonymous=False)
     my_nav = rospy.Publisher('/odom', Odometry, queue_size=50)
 
-    rate = rospy.Rate(100)
+    rate = rospy.Rate(100) # change from 100 to 50
     
     odom.header.stamp = rospy.Time.now()
     odom.header.frame_id = 'odom'
@@ -128,12 +79,29 @@ if __name__ == "__main__":
     old_l = 0
     old_r = 0
 
+    x_new = 0
+    y_new = 0
+    theta_new = 0
+
     while not rospy.is_shutdown():
         enc, _, _ = lenna.getOdometry()
         enc = getSigned(enc)
 
-        update_odom(enc[0], enc[1], enc[2], enc[3], old_l, old_r)
-        old_l, old_r = enc[2] ,enc[3]
+        if any(enc):
+            [x_new, y_new, theta_new] = update_odom(enc[0], enc[1], enc[2], enc[3], old_l, old_r)
+            old_l, old_r = enc[2] ,enc[3]
         
+
+        odom.header.stamp = rospy.Time.now()
+
+        odom.pose.pose.position.x = x_new 
+        odom.pose.pose.position.y = y_new
+        odom.pose.pose.position.z = 0
+
+        odom.pose.pose.orientation.x = 0
+        odom.pose.pose.orientation.y = 0
+        odom.pose.pose.orientation.z = np.sin(theta_new/2)
+        odom.pose.pose.orientation.w = np.cos(theta_new/2)
+
         my_nav.publish(odom)
         rate.sleep()
