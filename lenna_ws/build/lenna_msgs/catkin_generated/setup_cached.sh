3b4b089a90055baed8d8eff885a8a64318cebf3d
Erfan Riazati <erf.riazati@gmail.com>
2025-03-01 15:27:49 +0330

    🔧 [CONFIG] : ROS Builds and Catkin Packages

--- lenna_ws/build/lenna_msgs/catkin_generated/setup_cached.sh
+++ lenna_ws/build/lenna_msgs/catkin_generated/setup_cached.sh
@@ -9,6 +9,5 @@
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/lenna/repositories/Lenna-Mobile-Robot-ONE/lenna_ws/devel/.private/lenna_msgs:$CMAKE_PREFIX_PATH"
 export PWD='/home/lenna/repositories/Lenna-Mobile-Robot-ONE/lenna_ws/build/lenna_msgs'
-export PYTHONPATH='/opt/ros/melodic/lib/python2.7/dist-packages'
 export ROSLISP_PACKAGE_DIRECTORIES="/home/lenna/repositories/Lenna-Mobile-Robot-ONE/lenna_ws/devel/.private/lenna_msgs/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/home/lenna/repositories/Lenna-Mobile-Robot-ONE/lenna_ws/src/lenna_msgs:$ROS_PACKAGE_PATH"
