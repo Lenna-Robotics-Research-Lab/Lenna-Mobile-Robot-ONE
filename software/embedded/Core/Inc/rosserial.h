03ab0c953f68ecc4524532783599e4ae08aed4e5
arianhajiz <rian.hajizadeh@gmail.com>
2026-01-28 04:14:43 +0330

    ❇️ [add] : added the initial stable protocol
    this code works but it is very strange that when I delete the Transmits the code stops working I should work on this

---
+++ software/embedded/Core/Inc/rosserial.h
@@ -0,0 +1,64 @@
+/*
+ * rosserial.h
+ *
+ *  Created on: Jan 27, 2026
+ *      Author: arian
+ */
+
+#ifndef INC_ROSSERIAL_H_
+#define INC_ROSSERIAL_H_
+
+#include "usart.h"
+
+#define PACKET_HEADER_LENGTH	5	/**< Minimum expected packet length. */
+#define MAX_PACKET_LENGTH		255	/**< Maximum buffer size for a packet. */
+#define MAX_DATA_LENGTH			248
+
+/* Type Definitions ----------------------------------------------------------*/
+
+/**
+ * @struct packet_data
+ * @brief Stores velocity data from an incoming command packet.
+ */
+typedef struct
+{
+	int16_t right_velocity;	/**< Desired right wheel velocity. */
+	int16_t left_velocity;	/**< Desired left wheel velocity. */
+} packet_vel_data;
+
+/**
+ * @struct rosserial_cfgType
+ * @brief Stores configuration and state data for the packet handler module.
+ */
+typedef struct
+{
+	UART_HandleTypeDef *huart;						/**< Pointer to the UART handle for communication. */
+	uint8_t				min_pkt_len;				/**< Minimum packet length. */
+	uint8_t				max_pkt_len;				/**< Maximum packet length. */
+	uint8_t				dataValid;					/**< Flag to indicate if received data is valid (checksum passed). */
+	uint8_t				packetReceived;				/**< Flag to indicate a new packet has been received. */
+	uint8_t				headerValid;				/**< Flag to indicate that the received header is valid. */
+	uint16_t			data_len;					/**< Length of the packet after the header. */
+	uint16_t			pkt_len;					/**< Length of the whole packet. */
+	uint8_t 			rxbuffer[MAX_PACKET_LENGTH];	/**< Buffer to store incoming and outgoing data. */
+	uint8_t				data[MAX_DATA_LENGTH];						/**< data packet inside the buffer >*/
+	uint8_t 			txbuffer[MAX_PACKET_LENGTH];
+} rosserial_cfgType;
+
+/* Function Prototypes -------------------------------------------------------*/
+void LRL_ROSSerial_Init(rosserial_cfgType *rosserial_handle);
+
+void LRL_ROSSerial_Rx(rosserial_cfgType *rosserial_handle);
+
+void LRL_ROSSerial_Data_Handle(rosserial_cfgType *rosserial_handle);
+
+void _LRL_ROSSerial_Function(rosserial_cfgType *rosserial_handle);
+
+void LRL_ROSSerial_ReadAll(rosserial_cfgType *rosserial_handle);
+
+void LRL_ROSSerial_Query(rosserial_cfgType *rosserial_handle);
+//void LRL_ROSSerial_SetPID(rosserial_cfgType *rosserial_handle, pid_cfgType *pid_cfg);
+//
+//void LRL_ROSSerial_getPID(rosserial_cfgType *rosserial_handle, pid_cfgType *pid_cfg);
+
+#endif /* INC_ROSSERIAL_H_ */
---
+++ software/embedded/Core/Src/rosserial.c
@@ -0,0 +1,268 @@
+/**
+ * @file rosserial.c
+ * @brief This module handles the communication protocol for the robot.
+ *
+ * this function provides the functionality of rosserial on LCB of the robot
+ *
+ * @author Lenna Robotics Research Laboratory, Autonomous Systems Research Branch, Iran University of Science and Technology
+ * @date January 27, 2026
+ * @version 1.0
+ * @link https://github.com/Lenna-Robotics-Research-Lab
+ */
+
+/*-------------------------- Includes ---------------------------------------- */
+
+#include "rosserial.h"
+#include "usart.h"
+#include "stdlib.h"
+#include "string.h"
+//#include "pid.h"
+//#include "imu.h"
+//#include "odometry.h"
+
+/*-------------------------- Code Body ---------------------------------------- */
+
+
+void LRL_ROSSerial_Init(rosserial_cfgType *rosserial_handle)
+{
+	HAL_UART_Receive_IT(rosserial_handle->huart, rosserial_handle->rxbuffer, rosserial_handle->min_pkt_len);
+}
+
+
+void LRL_ROSSerial_Rx(rosserial_cfgType *rosserial_handle)
+{
+	if(!rosserial_handle->packetReceived)
+	{
+		if(rosserial_handle->rxbuffer[1] == 0xFE)
+		{
+			/*
+			 * version control for rosserial for ROS noetic or melodic.
+			 * this also would not allow invalid all zero packets pass
+			 */
+
+			uint8_t 	_checksum_calc = 0;
+			uint16_t	_temp;
+
+			_checksum_calc = 255 - ((rosserial_handle->rxbuffer[3]) + (rosserial_handle->rxbuffer[2]) % 256);
+
+			if(_checksum_calc == rosserial_handle->rxbuffer[4])
+			{
+				_temp	= ((uint16_t)(rosserial_handle->rxbuffer[3]<<8)) | (uint16_t)(rosserial_handle->rxbuffer[2]);
+				rosserial_handle->data_len	= _temp + 3; // this is the full packet length - the received initial 5
+				rosserial_handle->pkt_len 	= _temp + 8;
+
+				HAL_UART_Receive_IT(rosserial_handle->huart,
+								   &rosserial_handle->rxbuffer[rosserial_handle->min_pkt_len],
+								   rosserial_handle->data_len);
+
+				rosserial_handle->headerValid = 1;
+			}
+			else
+			{
+				/*
+				 * this would deal with unauthorized checksum headers
+				 */
+				rosserial_handle->headerValid = 0;
+			}
+			rosserial_handle->packetReceived = 1;
+		}
+		else
+		{
+			// Second stage: full packet received or invalid header
+		}
+	}
+	else
+	{
+		HAL_UART_Receive_IT(rosserial_handle->huart, rosserial_handle->rxbuffer, rosserial_handle->min_pkt_len);
+	}
+}
+
+void LRL_ROSSerial_Data_Handle(rosserial_cfgType *rosserial_handle)
+{
+    if(rosserial_handle->packetReceived)
+    {
+	    HAL_UART_Transmit(rosserial_handle->huart, "Stage 1", 7, 10);
+	    if(rosserial_handle->headerValid)
+	    {
+	    	HAL_UART_Transmit(rosserial_handle->huart, "Stage 2", 7, 10);
+		    memcpy(rosserial_handle->data, &rosserial_handle->rxbuffer[rosserial_handle->min_pkt_len], rosserial_handle->data_len);
+
+		    uint8_t _data_sum = 0, _data_checksum = 0;
+
+		    for(int i = 0; i<rosserial_handle->data_len; i++)
+		    {
+			    _data_sum 	+= rosserial_handle->data[i];
+		    }
+
+		    _data_checksum 	=  255 - (_data_sum % 256);
+
+		    if(_data_checksum == rosserial_handle->data[rosserial_handle->data_len])
+		    {
+		  	   HAL_UART_Transmit(rosserial_handle->huart, "Stage 3", 7, 10);
+		  	   rosserial_handle->dataValid = 1;
+		  	   _LRL_ROSSerial_Function(rosserial_handle);
+		  	   memset(rosserial_handle->rxbuffer, 0 , sizeof(rosserial_handle->rxbuffer));
+		  	   memset(rosserial_handle->data, 0 , sizeof(rosserial_handle->data));
+		    }
+		    else
+		    {
+		    	//something for second leg of data not being valid
+		    }
+
+
+		    rosserial_handle->headerValid = 0;
+	    }
+	    else
+	    {
+	    	// something to deal with data not being valid
+	    }
+	    rosserial_handle->packetReceived = 0;
+
+    }
+}
+
+void _LRL_ROSSerial_Function(rosserial_cfgType *rosserial_handle)
+{
+	uint8_t _id;
+
+	/*
+	 * in rosserial the functions are passed by two bytes of ID however here the number of functions
+	 * are limited to just 4. We did not want to change the overall packet frame of rosserial packet
+	 * hence we used the packeting as it was but we only use the low byte as the indicator of
+	 * our function ID.
+	 */
+	_id = rosserial_handle->data[0];
+	switch(_id)
+	{
+		case 0x00:
+			LRL_ROSSerial_Query(rosserial_handle);
+			break;
+//		case 0x01:
+//			LRL_ROSSerial_ReadAll(rosserial_handle);
+//		case 0x02:
+//			LRL_ROSSerial_SetPID(rosserial_handle, pid);
+//		case 0x03:
+//			LRL_ROSSerial_GetPID(rosserial_handle, pid);
+		default:
+			rosserial_handle->dataValid = 0;
+
+	}
+}
+
+void LRL_ROSSerial_Query(rosserial_cfgType *rosserial_handle)
+{
+	HAL_UART_Transmit(rosserial_handle->huart, rosserial_handle->rxbuffer, rosserial_handle->pkt_len, 10);
+	rosserial_handle->dataValid = 0;
+}
+
+/*
+void LRL_ROSSerial_ReadAll(rosserial_cfgType *rosserial_handle, odom_cfgType *odom, imu_statetype *imu)
+{
+	// Start of packet markers.
+	rosserial_handle->txbuffer[0] 	= 0xFF;
+	rosserial_handle->txbuffer[1] 	= 0xFE;
+
+	// Message data length.
+	rosserial_handle->txbuffer[2] 	= 0x1A; // low byte
+	rosserial_handle->txbuffer[3] 	= 0x00; // high byte
+
+	rosserial_handle->txbuffer[4] 	= 0xFF - 0x1A; // checksum for header validity
+
+	rosserial_handle->txbuffer[5] 	= 0x01; // ID low byte
+	rosserial_handle->txbuffer[6] 	= 0x00; // ID high byte
+	// Pack odometry data.
+	rosserial_handle->txbuffer[7] 	= (uint8_t)(odom->vel.left >> 8);
+	rosserial_handle->txbuffer[8] 	= (uint8_t)(odom->vel.left & 0x00FF);
+	rosserial_handle->txbuffer[9] 	= (uint8_t)(odom->vel.right >> 8);
+	rosserial_handle->txbuffer[10] 	= (uint8_t)(odom->vel.right & 0x00FF);
+	rosserial_handle->txbuffer[11] 	= (uint8_t)(odom->dist.left >> 8);
+	rosserial_handle->txbuffer[12] 	= (uint8_t)(odom->dist.left & 0x00FF);
+	rosserial_handle->txbuffer[13] 	= (uint8_t)(odom->dist.right >> 8);
+	rosserial_handle->txbuffer[14] 	= (uint8_t)(odom->dist.right & 0x00FF);
+
+	// Pack IMU accelerometer data.
+	rosserial_handle->txbuffer[15] 	= (uint8_t)(imu->accel.x_calibrated >> 8);
+	rosserial_handle->txbuffer[16] 	= (uint8_t)(imu->accel.x_calibrated & 0x00FF);
+	rosserial_handle->txbuffer[17] 	= (uint8_t)(imu->accel.y_calibrated >> 8);
+	rosserial_handle->txbuffer[18] 	= (uint8_t)(imu->accel.y_calibrated & 0x00FF);
+	rosserial_handle->txbuffer[19] 	= (uint8_t)(imu->accel.z_calibrated >> 8);
+	rosserial_handle->txbuffer[20] 	= (uint8_t)(imu->accel.z_calibrated & 0x00FF);
+
+	// Pack IMU gyroscope data.
+	rosserial_handle->txbuffer[21] 	= (uint8_t)(imu->gyro.x_calibrated >> 8);
+	rosserial_handle->txbuffer[22] 	= (uint8_t)(imu->gyro.x_calibrated & 0x00FF);
+	rosserial_handle->txbuffer[23] 	= (uint8_t)(imu->gyro.y_calibrated >> 8);
+	rosserial_handle->txbuffer[24] 	= (uint8_t)(imu->gyro.y_calibrated & 0x00FF);
+	rosserial_handle->txbuffer[25] 	= (uint8_t)(imu->gyro.z_calibrated >> 8);
+	rosserial_handle->txbuffer[26] 	= (uint8_t)(imu->gyro.z_calibrated & 0x00FF);
+
+	// Pack IMU angular position data.
+	rosserial_handle->txbuffer[27] 	= (uint8_t)(imu->angle.x >> 8);
+	rosserial_handle->txbuffer[28] 	= (uint8_t)(imu->angle.x & 0x00FF);
+	rosserial_handle->txbuffer[29] 	= (uint8_t)(imu->angle.y >> 8);
+	rosserial_handle->txbuffer[30] 	= (uint8_t)(imu->angle.y & 0x00FF);
+
+	// Pack magnetometer heading data.
+	rosserial_handle->txbuffer[31] 	= (uint8_t)(imu->mag.heading >> 8);
+	rosserial_handle->txbuffer[32] 	= (uint8_t)(imu->mag.heading & 0x00FF);
+
+	uint8_t _checksum = 0;
+
+	for(int i = 0; i < (0x02 + 0x1A) ; i++)
+	{
+		_checksum += rosserial_handle->txbuffer[i+5];
+	}
+
+	rosserial_handle->txbuffer[33] 	= _checksum;
+
+	// Transmit the complete packet.
+	HAL_UART_Transmit_IT(rosserial_handle->huart, rosserial_handle->txbuffer, 33);
+	memset(rosserial_handle->txbuffer, 0 , sizeof(rosserial_handle->txbuffer));
+	rosserial_handle->dataValid = 0;
+}
+
+void LRL_ROSSerial_SetPID(rosserial_cfgType *rosserial_handle, pid_cfgType *pid_cfg)
+{
+	pid_cfg->kp = rosserial_handle->data[2];
+	pid_cfg->ki = rosserial_handle->data[3];
+	pid_cfg->kd = rosserial_handle->data[4];
+
+	HAL_UART_Transmit(rosserial_handle->huart, rosserial_handle->rxbuffer, rosserial_handle->pkt_len, 1);
+	rosserial_handle->dataValid = 0;
+
+}
+
+void LRL_ROSSerial_GetPID(rosserial_cfgType *rosserial_handle, pid_cfgType *pid_cfg)
+{
+	// Start of packet markers.
+	rosserial_handle->txbuffer[0] 	= 0xFF;
+	rosserial_handle->txbuffer[1] 	= 0xFE;
+
+	// Message data length.
+	rosserial_handle->txbuffer[2] 	= 0x03; // low byte
+	rosserial_handle->txbuffer[3] 	= 0x00; // high byte
+
+	rosserial_handle->txbuffer[4] 	= 0xFF - 0x03;
+
+	rosserial_handle->txbuffer[5] 	= 0x02;
+	rosserial_handle->txbuffer[6] 	= 0x00;
+
+	rosserial_handle->txbuffer[7] 	= pid_cfg->kp;
+	rosserial_handle->txbuffer[8] 	= pid_cfg->ki;
+	rosserial_handle->txbuffer[9] 	= pid_cfg->kd;
+
+	uint8_t _checksum = 0;
+
+	for(int i = 0; i < (0x03 + 0x02) ; i++)
+	{
+		_checksum += rosserial_handle->txbuffer[i+5];
+	}
+
+	rosserial_handle->txbuffer[10]	= _checksum;
+
+	HAL_UART_Transmit_IT(rosserial_handle->huart, rosserial_handle->txbuffer, 10);
+	memset(rosserial_handle->txbuffer, 0 , sizeof(rosserial_handle->txbuffer));
+
+
+}
+*/
