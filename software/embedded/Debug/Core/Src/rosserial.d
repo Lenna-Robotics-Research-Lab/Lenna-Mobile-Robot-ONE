8a06218d8a80bfa5c85b3106a8ad38171c955cd6
arianhajiz <rian.hajizadeh@gmail.com>
2026-01-28 12:50:57 +0330

    📦 [pkg] : added build packages

--- software/embedded/Debug/Core/Src/rosserial.d
+++ software/embedded/Debug/Core/Src/rosserial.d
@@ -1,4 +1,5 @@
-Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
+Core/Src/rosserial.o: ../Core/Src/rosserial.c ../Core/Inc/rosserial.h \
+ ../Core/Inc/usart.h ../Core/Inc/main.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
  ../Core/Inc/stm32f4xx_hal_conf.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
@@ -34,14 +35,11 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h \
- ../Core/Inc/adc.h ../Core/Inc/main.h ../Core/Inc/eth.h ../Core/Inc/i2c.h \
- ../Core/Inc/spi.h ../Core/Inc/tim.h ../Core/Inc/usart.h \
- ../Core/Inc/gpio.h ../Core/Inc/ultrasonic.h ../Core/Inc/utilities.h \
- ../Core/Inc/tim.h ../Core/Inc/motion.h ../Core/Inc/pid.h \
- ../Core/Inc/odometry.h ../Core/Inc/i2c.h ../Core/Inc/motion.h \
- ../Core/Inc/mcu_config.h ../Core/Inc/packet_handler.h ../Core/Inc/imu.h \
- ../Core/Inc/usart.h ../Core/Inc/imu.h ../Core/Inc/rosserial.h \
- ../Core/Inc/pid.h ../Core/Inc/odometry.h
+ ../Core/Inc/pid.h ../Core/Inc/imu.h ../Core/Inc/i2c.h ../Core/Inc/tim.h \
+ ../Core/Inc/motion.h ../Core/Inc/odometry.h ../Core/Inc/usart.h \
+ ../Core/Inc/pid.h ../Core/Inc/imu.h ../Core/Inc/odometry.h
+../Core/Inc/rosserial.h:
+../Core/Inc/usart.h:
 ../Core/Inc/main.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
 ../Core/Inc/stm32f4xx_hal_conf.h:
@@ -78,27 +76,13 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
-../Core/Inc/adc.h:
-../Core/Inc/main.h:
-../Core/Inc/eth.h:
+../Core/Inc/pid.h:
+../Core/Inc/imu.h:
 ../Core/Inc/i2c.h:
-../Core/Inc/spi.h:
-../Core/Inc/tim.h:
-../Core/Inc/usart.h:
-../Core/Inc/gpio.h:
-../Core/Inc/ultrasonic.h:
-../Core/Inc/utilities.h:
 ../Core/Inc/tim.h:
 ../Core/Inc/motion.h:
-../Core/Inc/pid.h:
 ../Core/Inc/odometry.h:
-../Core/Inc/i2c.h:
-../Core/Inc/motion.h:
-../Core/Inc/mcu_config.h:
-../Core/Inc/packet_handler.h:
-../Core/Inc/imu.h:
 ../Core/Inc/usart.h:
-../Core/Inc/imu.h:
-../Core/Inc/rosserial.h:
 ../Core/Inc/pid.h:
+../Core/Inc/imu.h:
 ../Core/Inc/odometry.h:
